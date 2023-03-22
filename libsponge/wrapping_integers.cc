92ee5b991bf62ccd76cc0076800f1209857ec859
hxh <957627674@qq.com>
2023-03-22 01:10:07 +0000

    finish lab2 wrap and unwrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,5 @@
 #include "wrapping_integers.hh"
-
+#include<iostream>
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -15,7 +15,16 @@ using namespace std;
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
     DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+
+    uint64_t ISN(isn.raw_value());
+    n=n+ISN;
+    printf("%lu",n%UINT32_MAX);
+    cout<<n<<endl;
+    //operator<<(isn);
+    //cout<<isn;
+    n=n%(1ll << 32);//UINT32_MAX不行；
+
+    return WrappingInt32{static_cast<uint32_t>(n & 0xFFFFFFFF)};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -30,5 +39,48 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
     DUMMY_CODE(n, isn, checkpoint);
-    return {};
+
+    uint64_t ISN(isn.raw_value());
+    uint64_t N(n.raw_value());
+
+
+    //uint64_t diff=UINT64_MAX;
+
+    uint64_t BIG=1ll << 32;
+
+    while(N<ISN){
+
+        N=N+BIG;
+    }
+
+
+    uint64_t abs=N-ISN;
+
+    if(abs>=checkpoint)return abs;
+
+    uint64_t m=(checkpoint-abs)/BIG;//求一个大致的倍数;加快计算速度
+    
+    abs=abs+BIG*m;
+
+    while(checkpoint>=abs){
+
+        abs=abs+BIG;
+        //cout<<abs<<endl;
+
+    }
+
+        
+
+    if(abs<BIG)return abs;
+
+    if((abs-checkpoint)>=checkpoint-(abs-BIG)){
+
+       abs=abs-BIG;
+       
+    }
+
+    
+
+
+    return abs;
 }
